62afe12aa1a75b89db372e71fd6f19ed3b35714f
Juan I. Quiroga <juaniquiroga13@gmail.com>
2022-03-17 23:39:01 -0300

    hw1

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,35 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
-export default function Card(props) {
+export default function Card({ max, min, name, img, onClose }) {
   // acá va tu código
-  return <div>Card Component</div>
+  function handleOnChange() {
+    if(typeof onClose === "function") onClose();
+  }
+  
+  return (
+    <div>
+      <button onClick={handleOnChange}>X</button>
+      <span>{name}</span>
+      <div>
+        <div>
+          <span>Min</span>
+          <span>{min}</span>
+        </div>
+        <div>
+          <span>Max</span>
+          <span>{max}</span>
+        </div>
+        <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt="Weather Icon"/>
+      </div>
+    </div>
+  )
 };
+
+Card.propTypes = {
+  max: propTypes.number,
+  min: propTypes.number,
+  name: propTypes.string,
+  img: propTypes.string,
+  onClose: propTypes.func
+}
