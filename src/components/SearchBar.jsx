8ad9445a0261eea42dac0699a39afcf22f8c8da1
Juan I. Quiroga <juaniquiroga13@gmail.com>
2022-06-12 17:34:08 -0300

    functional

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import { IoSearchOutline } from 'react-icons/io5';
+import { useState } from 'react';
 import Styles from './SearchBar.module.css';
 
 export default function SearchBar({ onSearch }) {
+const [cityName, setCityName] = useState("");
 
   function handleOnSearch(event) {
     event.preventDefault();
     if(typeof onSearch === "function") {
-      const input = document.getElementById("search-bar-input");
-      onSearch(input.value);
-      input.value = "";
+      onSearch(cityName);
+      setCityName("");
     }
   }
 
   return (
     <form className={Styles.searchBar} onSubmit={handleOnSearch}>
-      <input placeholder="Add a new city" id="search-bar-input" />
+      <input placeholder="Add a new city" value={cityName} onChange={(e) => setCityName(e.target.value)} />
       <button type="submit"><IoSearchOutline /></button>
     </form>
   )
